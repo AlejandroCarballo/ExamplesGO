6876077ab6c042cbcc88cff641958ed10faa102c
AlejandroCarballo <o.alejandro.carballo@gmail.com>
2023-06-26 17:20:05 -0300

    functions

--- main.go
+++ main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
-	"github.com/AlejandroCarballo/ExamplesGO/variables"
+	"github.com/AlejandroCarballo/ExamplesGO/ejercicios"
 )
 
 func main() {
-	variables.RestoVariables()
+	/*variables.RestoVariables()
 	estado, texto := variables.ConviertoTexto(1500)
 	fmt.Println(estado)
+	fmt.Println(texto)
+
+	if os := runtime.GOOS; os == "linux." || os == "OS x." {
+		fmt.Println("Esto no es windows, es", os)
+
+	} else {
+		fmt.Println("Esto es windows")
+	}
+
+	switch os := runtime.GOOS; os {
+
+	case "linux":
+		fmt.Println("Esto es linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+
+	default:
+		fmt.Printf("%s \n", os)
+
+	}*/
+
+	numero, texto := ejercicios.Convertir("500")
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
